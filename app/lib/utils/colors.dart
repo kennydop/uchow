5e9f6af4d4eeef74b2a4a9b3f5aa64b02d9f1692
Patrick Danso Offei <dansooffeipatrick@gmail.com>
2022-02-27 13:08:38 +0000

    logo

--- app/lib/utils/colors.dart
+++ app/lib/utils/colors.dart
@@ -5,7 +5,7 @@ class AppColors {
   static const Color subTextColor = Color(0xFF7F7F7F);
   static const Color primaryBlackColor = Color(0xFF5A5A5A);
   static const Color primaryColor = Color(0xFFFF582D);
-  static const Color secondaryColor = Color(0xFFFFA52F);
+  static const Color secondaryColor = Color(0xFFFFCB1B);
   static const Color unselectedColor = Color(0xFF4F4F4F);
   static const Color bgColor = Color(0xFFFFFFFF);
   static const Color white = Color(0xFFFFFFFF);
